0284291216578c1c8e00758f6558a270bfed7a0a
anpohuni-debug <anpohuni@gmail.com>
2025-11-04 17:20:38 +0900

    Create a basic Streamlit app for user greeting
    Initial implementation of a Streamlit app that greets the user based on input.

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('나위 첫 스트림릿 앱')
+
+user_input = st.text_input('이름을 입력하세요')
+
+if st.button('인사하기')
+    if user_input:
+        st.write(f"안녕하세요, {user_input}님!")
+    else
+        st.wrtie("이름을 입력하세요.")
+
+
+
+        
+
+
